a4d762f4792cc23770ecd28d1613f66cfb6cbaeb
catsimple <catsimple@qq.com>
2024-06-18 22:38:31 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -130,6 +130,9 @@ git clone https://github.com/kongfl888/luci-app-adguardhome.git ./luci-app-adgua
 # # # # git clone --depth 1 https://github.com/fw876/helloworld && mv -n helloworld/luci-app-ssr-plus helloworld/naiveproxy ./ ; rm -rf helloworld
 # # # # git clone --depth 1 https://github.com/QiuSimons/openwrt-mos && mv -n openwrt-mos/luci-app-mosdns ./ ; rm -rf openwrt-mos
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2 passwall2 && mv -n passwall2/luci-app-passwall2 ./;rm -rf passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages openwrt-passwall-packages && mv -n openwrt-passwall-packages/ ./;rm -rf passwall2
+git clone --depth 1 https://github.com/catsimple/luci-app-temp-status_emb-h81b
+
 # # # # git clone --depth 1 -b luci https://github.com/xiaorouji/openwrt-passwall passwall && mv -n passwall/luci-app-passwall ./;rm -rf passwall
 # # # # git clone --depth 1 https://github.com/SSSSSimon/tencentcloud-openwrt-plugin-ddns && mv -n tencentcloud-openwrt-plugin-ddns/tencentcloud_ddns ./luci-app-tencentddns; rm -rf tencentcloud-openwrt-plugin-ddns
 # # # # git clone --depth 1 https://github.com/Tencent-Cloud-Plugins/tencentcloud-openwrt-plugin-cos && mv -n tencentcloud-openwrt-plugin-cos/tencentcloud_cos ./luci-app-tencentcloud-cos; rm -rf tencentcloud-openwrt-plugin-cos
